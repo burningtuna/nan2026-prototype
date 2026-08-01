efa49ba77015ff311f09cd3bce919281fcb2f809
burnintuna <burningtuna@gmail.com>
2026-08-02 06:01:28 +0900

    Render combat viewport at screen resolution

--- scripts/combat_hud_test.gd
+++ scripts/combat_hud_test.gd
@@ -7,20 +7,52 @@ const SIDEBAR_MAX_WIDTH := 192.0
 @onready var sidebar: PanelContainer = $Sidebar
 @onready var hud: GameHud = $Sidebar/HudStack/GameHud
 @onready var combat_container: SubViewportContainer = $CombatContainer
+@onready var combat_viewport: SubViewport = $CombatContainer/CombatViewport
 @onready var battle = $CombatContainer/CombatViewport/SampleAssembly
 @onready var overlay: CombatOverlay = $CombatContainer/CombatViewport/OverlayLayer/CombatOverlay
 
 
 func _ready() -> void:
 	resized.connect(_update_layout)
+	get_window().size_changed.connect(_update_layout)
 	_update_layout()
+	call_deferred("_update_layout")
 	call_deferred("_bind_combat")
 
 
 func _update_layout() -> void:
 	var sidebar_width := clampf(size.x * SIDEBAR_RATIO, SIDEBAR_MIN_WIDTH, SIDEBAR_MAX_WIDTH)
 	sidebar.offset_right = sidebar_width
-	combat_container.offset_left = sidebar_width + 1.0
+	var combat_position := Vector2(sidebar_width + 1.0, 0.0)
+	var logical_combat_size := Vector2(
+		maxf(size.x - combat_position.x, 1.0),
+		maxf(size.y, 1.0)
+	)
+	var render_scale := _screen_render_scale()
+	var render_size := Vector2i(
+		maxi(roundi(logical_combat_size.x * render_scale.x), 1),
+		maxi(roundi(logical_combat_size.y * render_scale.y), 1)
+	)
+
+	combat_container.position = combat_position
+	combat_container.size = Vector2(render_size)
+	combat_container.scale = Vector2(1.0 / render_scale.x, 1.0 / render_scale.y)
+	combat_viewport.size = render_size
+	combat_viewport.size_2d_override = Vector2i(
+		maxi(roundi(logical_combat_size.x), 1),
+		maxi(roundi(logical_combat_size.y), 1)
+	)
+	combat_viewport.size_2d_override_stretch = true
+
+
+func _screen_render_scale() -> Vector2:
+	var window_size := Vector2(DisplayServer.window_get_size())
+	if window_size.x <= 0.0 or window_size.y <= 0.0 or size.x <= 0.0 or size.y <= 0.0:
+		return Vector2.ONE
+	return Vector2(
+		maxf(window_size.x / size.x, 0.25),
+		maxf(window_size.y / size.y, 0.25)
+	)
 
 
 func _bind_combat() -> void:
